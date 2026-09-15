7a006874f0438c6db8c44787868463cbf82f42da
Cao Lê Gia Phú <23127535@student.hcmus.edu.vn>
2026-09-15 15:00:40 +0700

    test ci.yml

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -76,7 +76,7 @@ export default function Login({ onOk, onGoRegister }) {
           </div>
         )}
 
-        <div className="mt-5 text-xs text-slate-400">© CMC BANKING</div>
+        <div className="mt-5 text-xs text-slate-400">© CMC BANKING v.01</div>
       </div>
     </div>
   );
